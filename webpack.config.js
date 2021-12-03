b2f9bdbeee68448a15516ba5566af1b9f42a7172
Zlatin Hristov <zlatin.hristov@giftcards.eu>
2021-12-03 14:30:12 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	resolve: {
+		fallback: {
+			buffer: require.resolve('buffer'),
+			stream: require.resolve('stream'),
+			crypto: require.resolve('crypto-browserify')
+		}
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'crypto.js',
+		library: 'loadScriptAsync',
+		libraryTarget: 'umd',
+		globalObject: 'this',
+		umdNamedDefine: true
+	}
+};
